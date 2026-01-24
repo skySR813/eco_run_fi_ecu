fc3e1934f5022194001ad3aa8b12d899a7262c44
skySR813 <ryosa0490@gmail.com>
2026-01-24 19:23:11 +0900

    add ig system

--- main_ecu/Core/Src/main.c
+++ main_ecu/Core/Src/main.c
@@ -12,6 +12,11 @@
   * This software is licensed under terms that can be found in the LICENSE file
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
+  * この順で保存
+  * git status
+    git add .
+    git commit -m "tune injection map"
+    git push
   *
   ******************************************************************************
   */
